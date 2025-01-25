5dd75343efdf8d7bf8f20da526c0601c1710fbdb
Sourav Goswami <132908798+sourav-iiitd@users.noreply.github.com>
2025-01-25 22:41:11 +0530

    Add files via upload

---
+++ Health_Association_Analysis/(Others) Non-Industrialized_Analysis/Scripts/c70_new_UrbanRuralMixed_Info_data.R
@@ -0,0 +1,139 @@
+## Replicating c70_ValidationDiseaseSampleInfo.R for UrbanRuralMixed cohort
+
+# Omprakash
+# 25-09-2024
+
+# the code will get the information of all the diseases and their control and disease samples for UrbanRuralMixed cohort
+
+
+## Load the UrbanRuralMixed_metadata 
+c1_new_cohort_wise_master_data <- new.env()
+load("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/c1_new_cohort_wise_master_data.RData",envir = c1_new_cohort_wise_master_data)
+attach(c1_new_cohort_wise_master_data)
+UrbanRuralMixed_metadata <- UrbanRuralMixed_metadata
+detach(c1_new_cohort_wise_master_data)
+rm(c1_new_cohort_wise_master_data)
+
+library(dplyr)
+library(tidyverse)
+library(writexl)
+
+expandedMetadata= UrbanRuralMixed_metadata %>% 
+  filter(study_condition=="disease") %>% 
+  separate_rows(diseaseCat, sep=";") %>% 
+  filter(diseaseCat!="other")
+
+uniqueDiseases = unique(expandedMetadata$diseaseCat)
+
+# getting total rows for the disease study combinations
+rowCount=0
+
+for(disease in uniqueDiseases)
+{
+  # getting all the samples for the given disease.
+  temp= expandedMetadata[expandedMetadata$diseaseCat==disease,]
+  totalStudies= length(unique(temp$study_name))
+  rowCount= rowCount+totalStudies
+}
+
+# creating a empty dataframe that will store control and disease sample count for each of the disease__study combination.
+UrbanRuralMixedCohortInfo= as.data.frame(matrix(nrow= rowCount, ncol= 4))
+colnames(UrbanRuralMixedCohortInfo)= c("disease", "study", "controlcount", "diseaseCount")
+
+cnt= 1
+for(disease in uniqueDiseases)
+{
+  # getting disease specific samples
+  temp= expandedMetadata[expandedMetadata$diseaseCat==disease,]
+  
+  # getting the studies that have the given disease
+  uniqueStudies= unique(temp$study_name)
+  
+  for(study in uniqueStudies)
+  {
+    # creating a vector that will contain the information about the current disease__study combination.
+    currRow= c(disease, study)
+    
+    # getting the number of non-diseases (control) samples for the given study.
+    controlMeta= UrbanRuralMixed_metadata %>% 
+      filter(study_name==study) %>% 
+      filter(study_condition== "control")
+    
+    controlCount= nrow(controlMeta)
+    
+    # disease sample count in the specific study
+    diseaseCount= nrow(temp[temp$study_name==study,])
+    
+    currRow= c(currRow,controlCount)
+    currRow= c(currRow, diseaseCount)
+    
+    UrbanRuralMixedCohortInfo[cnt,]= currRow
+    cnt= cnt+1
+  }
+  
+}
+
+UrbanRuralMixedCohortInfo$controlcount= as.numeric(UrbanRuralMixedCohortInfo$controlcount)
+UrbanRuralMixedCohortInfo$diseaseCount= as.numeric(UrbanRuralMixedCohortInfo$diseaseCount)
+
+# getting only those disease_study combinations that has the corresponding control match samples.
+UrbanRuralMixedCohortInfo= UrbanRuralMixedCohortInfo %>% 
+  filter(controlcount>0) %>% 
+  arrange(controlcount)
+
+UrbanRuralMixedCohortInfo= UrbanRuralMixedCohortInfo %>% 
+  arrange(disease)
+
+# creating columns that will store the infromation about the percentage of control and disease samples for the given combination. 
+# It is done on the disease__study combinations instead of the all the samples for the given disease condition from all the studies having that disease becase
+# we are considering control matched samples for the disease in each study. If the given study has significnatly large number control and disease samples then 
+# that may generate the bias. Therefore, for each study there should be enough disease and control matched samples independently.
+
+# here the 60,70,80 mean that the control sample percent or disease sample percent for the particular disease__study combination doesn't excede these numbers.
+# simply neither of them can constitute 61%,71%, 81% of the total number of samples.
+UrbanRuralMixedCohortInfo$`60_40`= NA
+UrbanRuralMixedCohortInfo$`70_30`= NA
+UrbanRuralMixedCohortInfo$`80_20`= NA
+
+for(index in 1:nrow(UrbanRuralMixedCohortInfo))
+{
+  total= UrbanRuralMixedCohortInfo[index,"controlcount"]  + UrbanRuralMixedCohortInfo[index, "diseaseCount"]
+  value= UrbanRuralMixedCohortInfo[index,"controlcount"]/total
+  
+  if(value<0.4 | value>0.6)
+  {
+    UrbanRuralMixedCohortInfo[index,"60_40"]=0
+  }
+  else
+  {
+    UrbanRuralMixedCohortInfo[index,"60_40"]=1
+  }
+  
+  
+  if(value<0.3 | value>0.7)
+  {
+    UrbanRuralMixedCohortInfo[index,"70_30"]=0
+  }
+  
+  else
+  {
+    UrbanRuralMixedCohortInfo[index,"70_30"]=1
+  }
+  
+  if(value<0.2 | value>0.8)
+  {
+    UrbanRuralMixedCohortInfo[index,"80_20"]=0
+  }
+  
+  else
+  {
+    UrbanRuralMixedCohortInfo[index,"80_20"]=1
+  }
+  
+  
+}
+
+write_xlsx(UrbanRuralMixedCohortInfo, "C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/UrbanRuralMixed/c70_new_UrbanRuralMixed_Info.xlsx")
+
+
+save.image("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/UrbanRuralMixed/c70_new_UrbanRuralMixed_Info_data.RData")
---
+++ Health_Association_Analysis/(Others) Non-Industrialized_Analysis/Scripts/c80_new_UrbanRuralMixed_Iterative_disease_analysis.R
@@ -0,0 +1,101 @@
+######### This code is a replication of c80_Aggregated_iterativeCohortAnalysis.R with some modifications for UrbanRuralMixed
+## Omprakash 
+## Dt.25-09-2024
+
+
+## This code will use c73 for UrbanRuralMixed cohort and run disease analysis for this cohort
+
+############################################################################## Load the data
+## Load the UrbanRuralMixed_metadata and UrbanRuralMixed_spProfile 
+c1_new_cohort_wise_master_data <- new.env()
+load("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/c1_new_cohort_wise_master_data.RData",envir = c1_new_cohort_wise_master_data)
+attach(c1_new_cohort_wise_master_data)
+UrbanRuralMixed_metadata <- UrbanRuralMixed_metadata
+UrbanRuralMixed_spProfile <- UrbanRuralMixed_spProfile
+detach(c1_new_cohort_wise_master_data)
+rm(c1_new_cohort_wise_master_data)
+
+## Load the functions that are used in disease analysis (c73_new_UrbanRuralMixed_disease_analysis_functions.R)
+source("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/UrbanRuralMixed/c73_new_UrbanRuralMixed_disease_analysis_functions.R")
+## Load the species to check in the data 
+load("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/NewGutAssociatedSpecies.RData")
+
+## Load the summarised information for UrbanRuralMixed cohort from c70 (c70_new_UrbanRuralMixed_Info_data.RData)
+c70_new_UrbanRuralMixed_Info_data <- new.env()
+load("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/UrbanRuralMixed/c70_new_UrbanRuralMixed_Info_data.RData",envir = c70_new_UrbanRuralMixed_Info_data)
+attach(c70_new_UrbanRuralMixed_Info_data)
+UrbanRuralMixedCohortInfo <- UrbanRuralMixedCohortInfo
+detach(c70_new_UrbanRuralMixed_Info_data)
+rm(c70_new_UrbanRuralMixed_Info_data)
+
+
+################################################################################ Now extract the data for input for functions
+## filtering out the disease__study combinations 
+UrbanRuralMixedCohortInfo= UrbanRuralMixedCohortInfo %>% 
+  filter(`80_20`==1) %>% 
+  filter(controlcount>=15 & diseaseCount>=15)
+
+# getting the control samples of the shortlisted studies for the discovery cohort.
+controlSampleList= list()
+uniqueStudies= unique(UrbanRuralMixedCohortInfo$study)
+
+# storing the control samples for all the studies in the list.
+for(study in uniqueStudies)
+{
+  currStudy= UrbanRuralMixed_metadata %>% 
+    filter(study_name==study) %>% 
+    filter(study_condition=="control")
+  
+  controlSampleList[[study]]= currStudy$sample_id
+}
+
+# expanding the metadata based on disease category
+expandedMetadata= UrbanRuralMixed_metadata %>% 
+  separate_rows(diseaseCat, sep=";")
+
+allDiseases= unique(UrbanRuralMixedCohortInfo$disease)  
+
+# it will contain the sample_ids for the disease__study combinations
+diseaseSampleList= list()
+
+for(dis in allDiseases)
+{
+  temp= UrbanRuralMixedCohortInfo %>% 
+    filter(disease==dis)
+  
+  uniqueStudies= unique(temp$study)
+  
+  for(study in uniqueStudies)
+  {
+    tempMeta= expandedMetadata %>% 
+      filter(study_name==study) %>% 
+      filter(diseaseCat==dis)
+    
+    diseaseSampleList[[dis]][[study]]= tempMeta$sample_id
+  }
+  
+}
+
+################################################################################ Now Run the functions for Disease Analysis
+
+MainOutputFolder= "C:\\Users\\ompra\\Downloads\\Cell_reports_revision\\disease_analysis\\new_files\\UrbanRuralMixed\\disease_analysis_results"
+
+#Extrcat the intersect species to check
+filtered_NewGutAssociatedSpecies <- sort(intersect(colnames(UrbanRuralMixed_spProfile),NewGutAssociatedSpecies))
+
+UrbanRuralMixed_iterative_disease<- diseaseAnalysisIterationPipeline(10, unique(UrbanRuralMixedCohortInfo$disease), controlSampleList, diseaseSampleList, MainOutputFolder, UrbanRuralMixed_spProfile, filtered_NewGutAssociatedSpecies)
+
+################################################################################# Get the Mean Health score for each of the species
+
+iterationHealthScore_rankedScaledDifference= as.data.frame(UrbanRuralMixed_iterative_disease[["iterationOutputDf_ranked_scaledDifference"]])
+
+UrbanRuralMixed_HealthScore = as.data.frame(apply(iterationHealthScore_rankedScaledDifference,1,mean))
+colnames(UrbanRuralMixed_HealthScore) <- "health_score"
+
+################################################################################# Save the workspace and individual Health Score
+
+save(UrbanRuralMixed_HealthScore, file = "C:\\Users\\ompra\\Downloads\\Cell_reports_revision\\disease_analysis\\new_files\\UrbanRuralMixed\\disease_analysis_results\\UrbanRuralMixed_HealthScore.RData")
+
+save.image("C:/Users/ompra/Downloads/Cell_reports_revision/disease_analysis/new_files/UrbanRuralMixed/c80_new_UrbanRuralMixed_Iterative_disease_analysis_workspace.RData")
+
+################################################################################  Do the correlation of health score for UrbanRuralMixed with the health score of overall Data (Previously done by Abhishek)
